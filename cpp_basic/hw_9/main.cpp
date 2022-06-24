8e67cb733e8ca2c8e7a8976efba7b6758637f615
Alexandr Novichkov <alexandr.novichkov@ncloudtech.ru>
2022-06-24 20:12:29 +0300

    OT-8 | feat: hw 9

---
+++ cpp_basic/hw_9/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <limits>
+#include "stat.cpp"
+
+int main()
+{
+
+	const size_t statistics_count = 4;
+	IStatistics *statistics[statistics_count];
+
+	statistics[0] = new Min{};
+	statistics[1] = new Max{};
+	statistics[2] = new Mean{};
+	statistics[3] = new Std{};
+
+	double val = 0;
+	while (std::cin >> val)
+	{
+		for (size_t i = 0; i < statistics_count; ++i)
+		{
+			statistics[i]->update(val);
+		}
+	}
+
+	// Handle invalid input data
+	if (!std::cin.eof() && !std::cin.good())
+	{
+		std::cerr << "Invalid input data\n";
+		return 1;
+	}
+
+	// Print results if any
+	for (size_t i = 0; i < statistics_count; ++i)
+	{
+		std::cout << statistics[i]->name() << " = " << statistics[i]->eval() << std::endl;
+	}
+
+	// Clear memory - delete all objects created by new
+	for (size_t i = 0; i < statistics_count; ++i)
+	{
+		delete statistics[i];
+	}
+
+	return 0;
+}

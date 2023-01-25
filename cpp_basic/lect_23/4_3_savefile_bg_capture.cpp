b2009c9f7611797a7429822a3c375d9e9519fb0f
Alexandr Novichkov <alexandr.novichkov@ncloudtech.ru>
2023-01-25 23:43:00 +0300

    OT-23 | feat: lect 23

--- cpp_basic/lect_23/4_3_savefile_bg_capture.cpp
+++ cpp_basic/lect_23/4_3_savefile_bg_capture.cpp
@@ -26,10 +26,21 @@ std::thread savefile_bg(const std::string &filename,
   return std::thread{std::move(write)};
 }
 
+std::thread savefile_bg_2(const std::string &filename,
+                          const std::vector<char> &buffer) {
+  return std::thread{[&filename, &buffer]() {
+    std::ofstream out{filename};
+    out.write(buffer.data(), static_cast<std::streamsize>(buffer.size()));
+    out.close();
+
+    std::clog << "File written." << std::endl;
+  }};
+}
+
 int main() {
   const std::vector<char> buffer(10 * 4096 * 4096, 'A');
-  std::thread bg_task = savefile_bg(null, buffer);
 
+  std::thread bg_task = savefile_bg(null, buffer);
   bg_task.detach();
 
   std::getchar();

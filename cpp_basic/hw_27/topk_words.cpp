0f77fb7d0e49a0df199a16741ce50427b099e4d0
Alexandr Novichkov <alexandr.novichkov@ncloudtech.ru>
2023-02-03 00:35:27 +0300

    OT-27 | feat: hw 27

--- cpp_basic/hw_27/topk_words.cpp
+++ cpp_basic/hw_27/topk_words.cpp
@@ -4,11 +4,14 @@
 #include <cctype>
 #include <chrono>
 #include <cstdlib>
+#include <execution>
 #include <fstream>
+#include <future>
 #include <iomanip>
 #include <iostream>
 #include <iterator>
 #include <map>
+#include <thread>
 #include <vector>
 
 const size_t TOPK = 10;
@@ -21,6 +24,10 @@ void count_words(std::istream &stream, Counter &);
 
 void print_topk(std::ostream &stream, const Counter &, const size_t k);
 
+Counter join_map(const Counter &counter1, const Counter &counter2);
+
+Counter get_file_and_count(const std::string &filepath);
+
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     std::cerr << "Usage: topk_words [FILES...]\n";
@@ -29,13 +36,19 @@ int main(int argc, char *argv[]) {
 
   auto start = std::chrono::high_resolution_clock::now();
   Counter freq_dict;
+  std::vector<std::future<Counter>> tasks;
+
   for (int i = 1; i < argc; ++i) {
-    std::ifstream input{argv[i]};
-    if (!input.is_open()) {
-      std::cerr << "Failed to open file " << argv[i] << '\n';
+    tasks.push_back(std::async(get_file_and_count, argv[i]));
+  }
+
+  for (size_t i = 0; i < tasks.size(); ++i) {
+    try {
+      freq_dict = join_map(freq_dict, tasks[i].get());
+    } catch (std::exception &e) {
+      std::cerr << "Error: " << e.what() << std::endl;
       return EXIT_FAILURE;
     }
-    count_words(input, freq_dict);
   }
 
   print_topk(std::cout, freq_dict, TOPK);
@@ -43,6 +56,29 @@ int main(int argc, char *argv[]) {
   auto elapsed_ms =
       std::chrono::duration_cast<std::chrono::microseconds>(end - start);
   std::cout << "Elapsed time is " << elapsed_ms.count() << " us\n";
+}
+
+Counter get_file_and_count(const std::string &filepath) {
+  std::ifstream input{filepath};
+  if (!input.is_open()) {
+    throw std::runtime_error{"Failed to open file '" + filepath + "'"};
+  }
+
+  Counter freq_dict;
+  count_words(input, freq_dict);
+
+  return freq_dict;
+}
+
+Counter join_map(const Counter &counter1, const Counter &counter2) {
+  Counter joined_counter;
+  for (const auto &[word, freq] : counter2) {
+    joined_counter[word] += freq;
+  }
+  for (const auto &[word, freq] : counter1) {
+    joined_counter[word] += freq;
+  }
+  return joined_counter;
 }
 
 std::string tolower(const std::string &str) {

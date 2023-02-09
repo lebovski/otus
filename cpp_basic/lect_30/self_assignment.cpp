f96f1c5cfc0995fb9dc2dbeaeb28bef86c4160b6
Alexandr Novichkov <alexandr.novichkov@ncloudtech.ru>
2023-02-09 22:57:29 +0300

    OT-30 | feat: lect

--- cpp_basic/lect_30/self_assignment.cpp
+++ cpp_basic/lect_30/self_assignment.cpp
@@ -3,152 +3,130 @@
 // self assignment check
 
 namespace motivation {
+struct some_struct {
+  some_struct(size_t size) : m_data{new int[size]}, m_size{size} {}
+  ~some_struct() { delete[] m_data; }
 
-	struct some_struct {
+  some_struct(const some_struct &rhs) {
+    m_data = new int[rhs.m_size];
+    m_size = rhs.m_size;
+  }
 
-		some_struct(size_t size)
-			: m_data{new int[size]}, m_size{size} {}
-		~some_struct() {
-			delete[] m_data;
-		}
+  some_struct &operator=(const some_struct &rhs) {
+    delete[] m_data;
+    // What if *this == rhs?
+    // We are in trouble
+    m_data = new int[rhs.m_size];
+    m_size = rhs.m_size;
+    return *this;
+  }
 
-		some_struct(const some_struct &rhs) {
-			m_data = new int[rhs.m_size];
-			m_size = rhs.m_size;
-		}
+private:
+  size_t m_size;
+  int *m_data;
+};
 
-		some_struct &operator=(const some_struct &rhs) {
-			delete[] m_data;
-			// What if *this == rhs?
-			// We are in trouble
-			m_data = new int[rhs.m_size];
-			m_size = rhs.m_size;
-			return *this;
-		}
+void example() {
+  std::cout << "motivation::example" << std::endl;
 
-	private:
-		size_t m_size;
-		int *m_data;
-	};
+  some_struct some{42};
+  some = some; // Ooops
 
-	void example() {
-		std::cout << "motivation::example" << std::endl;
-
-		some_struct some{42};
-		some = some; // Ooops
-
-		std::cout << std::endl
-				  << std::endl;
-	}
+  std::cout << std::endl << std::endl;
+}
 
 } // namespace motivation
 
 namespace copy_and_swap {
+struct some_class {
+  some_class(int data_size) : m_size{data_size}, m_data{new int[data_size]} {}
+  ~some_class() { delete[] m_data; }
 
-	struct some_class {
+  some_class(const some_class &other) {
+    m_size = other.m_size;
+    m_data = new int[m_size];
+    // copy data here
+  }
 
-		some_class(int data_size)
-			: m_size{data_size}, m_data{new int[data_size]} {
-		}
-		~some_class() {
-			delete[] m_data;
-		}
+  some_class &operator=(const some_class &other) {
+    // create new object from other
+    some_class tmp{other}; // allocation
+    // if exception here, current object is still valid
+    // swap - this operation could not throw
+    swap(*this, tmp);
+    return *this;
+  }
 
-		some_class(const some_class &other) {
-			m_size = other.m_size;
-			m_data = new int[m_size];
-			// copy data here
-		}
+private:
+  void swap(some_class &lhs, some_class &rhs) {
+    std::swap(lhs.m_data, rhs.m_data);
+    std::swap(lhs.m_size, rhs.m_size);
+  }
 
-		some_class &operator=(const some_class &other) {
-			// create new object from other
-			some_class tmp{other}; // allocation
-			// if exception here, current object is still valid
-			// swap - this operation could not throw
-			swap(*this, tmp);
-			return *this;
-		}
+  int m_size;
+  int *m_data;
+};
 
-	private:
-		void swap(some_class &lhs, some_class &rhs) {
-			std::swap(lhs.m_data, rhs.m_data);
-			std::swap(lhs.m_size, rhs.m_size);
-		}
+void example() {
+  std::cout << "copy_and_swap::example" << std::endl;
 
-		int m_size;
-		int *m_data;
-	};
+  some_class some{42};
+  // Good now
+  some = some;
+  // But need additional allocation
 
-	void example() {
-		std::cout << "copy_and_swap::example" << std::endl;
+  std::cout << std::endl << std::endl;
+}
 
-		some_class some{42};
-		// Good now
-		some = some;
-		// But need additional allocation
-
-		std::cout << std::endl
-				  << std::endl;
-	}
-
-} // copy_and_swap
+} // namespace copy_and_swap
 
 namespace self_check {
+struct some_class {
+  some_class(int data_size) : m_size{data_size}, m_data{new int[data_size]} {}
+  ~some_class() { delete[] m_data; }
 
-	struct some_class {
+  some_class(const some_class &other) {
+    m_size = other.m_size;
+    m_data = new int[m_size];
+    // copy data here
+  }
 
-		some_class(int data_size)
-			: m_size{data_size}, m_data{new int[data_size]} {
-		}
-		~some_class() {
-			delete[] m_data;
-		}
+  some_class &operator=(const some_class &other) {
+    if (this != &other) {
+      // create new object from other
+      some_class tmp{other};
+      // if exception here, current object is still valid
+      // swap - this operation could not throw
+      swap(*this, tmp);
+    }
+    return *this;
+  }
 
-		some_class(const some_class &other) {
-			m_size = other.m_size;
-			m_data = new int[m_size];
-			// copy data here
-		}
+private:
+  void swap(some_class &lhs, some_class &rhs) {
+    std::swap(lhs.m_data, rhs.m_data);
+    std::swap(lhs.m_size, rhs.m_size);
+  }
 
-		some_class &operator=(const some_class &other) {
-			if (this != &other) {
-				// create new object from other
-				some_class tmp{other};
-				// if exception here, current object is still valid
-				// swap - this operation could not throw
-				swap(*this, tmp);
-			}
-			return *this;
-		}
+  int m_size;
+  int *m_data;
+};
 
-	private:
-		void swap(some_class &lhs, some_class &rhs) {
-			std::swap(lhs.m_data, rhs.m_data);
-			std::swap(lhs.m_size, rhs.m_size);
-		}
+void example() {
+  std::cout << "self_check::example" << std::endl;
 
-		int m_size;
-		int *m_data;
-	};
+  some_class some{42};
+  // Very good now
+  some = some;
 
-	void example() {
-		std::cout << "self_check::example" << std::endl;
-
-		some_class some{42};
-		// Very good now
-		some = some;
-
-		std::cout << std::endl
-				  << std::endl;
-	}
-
-} // self_check
+  std::cout << std::endl << std::endl;
+}
+} // namespace self_check
 
 int main() {
+  motivation::example();
+  copy_and_swap::example();
+  self_check::example();
 
-	motivation::example();
-	copy_and_swap::example();
-	self_check::example();
-
-	return 0;
+  return 0;
 }
